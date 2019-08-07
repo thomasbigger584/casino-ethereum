fd189ed359ced6a930b36d8b1d8cfca92c045985
Thomas Bigger <thomas@performanceactive.com>
2019-08-07 14:15:06 +0100

    using yeoman generator project insead of basic

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,8 @@
 var Casino = artifacts.require("./Casino.sol");
 
 
-// minimum bet is 0.1 ether converted to wei 
+// minimum bet is 0.1 ether converted to wei
+// gas limit that we are willing to use to deploy the contract
 module.exports = function(deployer) {
   deployer.deploy(web3.toWei(0.1, 'ether'), 100, {gas: 3000000});
 };
